8cd45ef4c06e483708cadd904bb8565df576c756
Pavel <pav007prikhod@mail.ru>
2019-05-18 11:33:39 +0700

    GET POST REQUESTS

--- server/index/views.py
+++ server/index/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
  
+@csrf_exempt
 def index(request):
-    header = "Денчик слазиет"                    # обычная переменная
-    data = {"header": header}
-    return render(request, "index.html", context=data)
+	if(request.method == 'POST'):
+		received_json_data=json.loads(request.body)
+		print(received_json_data)
+		return render(request, "index.html", context=received_json_data)
+	
+	else:
+		header = "Денчик слазиет"                    			# обычная переменная
+		data = {"header": header}
+		return render(request, "index.html", context=data)
