f6fabbedeb56f412a25be4e8e9136a8f0117b3c5
Pavel <pav007prikhod@mail.ru>
2019-05-18 13:48:03 +0700

    All displaying + GET + POST

--- server/index/views.py
+++ server/index/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+import redis
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
  
@@ -7,10 +8,20 @@ from django.http import HttpResponseRedirect
 def index(request):
 	if(request.method == 'POST'):
 		received_json_data=json.loads(request.body)
-		print(received_json_data)
+		pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+		r = redis.Redis(connection_pool=pool)
+		key = received_json_data.get('name')
+		value = received_json_data.get('surname')
+		r.set(key, value)
 		return render(request, "index.html", context=received_json_data)
 	
 	else:
-		header = "Денчик слазиет"                    			# обычная переменная
+		pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+		r = redis.Redis(connection_pool=pool)
+		for key in r.keys():
+			print(key)
+			print(r.get(key))
+			print()
+		header = "Денчик слазиет"
 		data = {"header": header}
 		return render(request, "index.html", context=data)
